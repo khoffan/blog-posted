a3c3920787d8e7049af43f1796627263f68bed63
khoffan <khoffan222@gmail.com>
2024-03-08 03:30:37 +0700

    first-commit

---
+++ server-blog-posted/index.js
@@ -0,0 +1,19 @@
+// build express example code
+
+const express = require('express')
+const app = express()
+const cors = require("cors")
+const bodyparser = require("body-parser"); 
+const port = 3000
+
+app.use(cors())
+app.use(express.json)
+app.use(bodyparser.json())  // support json encoded bodies
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
