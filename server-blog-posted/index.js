96058ebd352bb20144f2e2eca33a78a163ae1982
khoffan <khoffan222@gmail.com>
2024-03-08 03:35:52 +0700

    change defuilt

--- server-blog-posted/index.js
+++ server-blog-posted/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 const bodyparser = require("body-parser"); 
-const port = 3000
+const port = 4000
 
 app.use(cors())
 app.use(express.json)
