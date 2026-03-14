c59b8d2371589bfda3a28a626902218de99af654
khoffan <khoffan222@gmail.com>
2026-03-14 21:47:06 +0700

    chore manage firebase config

--- server-blog-posted/config/firebase_config_admin.js
+++ server-blog-posted/config/firebase_config_admin.js
@@ -1,9 +1,19 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../firebase-service.json");
+
+const isProd = process.env.NODE_ENV === "production" ? true : false;
+
+let serviceParse;
+if(isProd) {
+  const serviceEnv = process.env.FIREBASE_SERVICE;
+  serviceParse = JSON.parse(serviceEnv);
+} else {
+  const serviceAccount = require("../firebase-service.json");
+  serviceParse = serviceAccount;
+}
 
 const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceParse)
 });
 
 module.exports = app;
